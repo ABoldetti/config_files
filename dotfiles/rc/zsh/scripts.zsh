0b3281c079d814d4dd02a71d2075258b1e3f28b0
ABoldetti <a.boldetti@campus.unimib.it>
2026-07-01 18:35:28 +0200

    rc

---
+++ dotfiles/rc/zsh/scripts.zsh
@@ -0,0 +1,10 @@
+export SCRIPT=$HOME/coding/config_files/scripts
+export ZSHDIR=$HOME/coding/config_files/dotfiles/rc/zsh
+alias gaming="$SCRIPT/gaming" 
+
+
+ss(){
+$SCRIPT/screen_switcher $1
+}
+
+fastfetch
